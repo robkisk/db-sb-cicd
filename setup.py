bf703271425c7724b7f55ddcb99f24bc289fa271
robkisk <robby.kiskanyan@databricks.com>
2023-05-12 07:46:35 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+"""
+This file configures the Python package with entrypoints used for future runs on Databricks.
+
+Please follow the `entry_points` documentation for more details on how to configure the entrypoint:
+* https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+"""
+
+from setuptools import find_packages, setup
+from db_sb_cicd import __version__
+
+PACKAGE_REQUIREMENTS = ["pyyaml"]
+
+# packages for local development and unit testing
+# please note that these packages are already available in DBR, there is no need to install them on DBR.
+LOCAL_REQUIREMENTS = [
+    "pyspark==3.2.1",
+    "delta-spark==1.1.0",
+    "scikit-learn",
+    "pandas",
+    "mlflow",
+]
+
+TEST_REQUIREMENTS = [
+    # development & testing tools
+    "pytest",
+    "coverage[toml]",
+    "pytest-cov",
+    "dbx>=0.8",
+    "black",
+]
+
+setup(
+    name="db_sb_cicd",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    setup_requires=["setuptools", "wheel"],
+    install_requires=PACKAGE_REQUIREMENTS,
+    extras_require={"local": LOCAL_REQUIREMENTS, "test": TEST_REQUIREMENTS},
+    entry_points={
+        "console_scripts": [
+            "etl = db_sb_cicd.tasks.sample_etl_task:entrypoint",
+            "ml = db_sb_cicd.tasks.sample_ml_task:entrypoint",
+        ]
+    },
+    version=__version__,
+    description="",
+    author="",
+)
